f6fad5de931792075f2b369ce298c38769d3c8da
artem <bakirovartem69@gmail.com>
2022-10-07 18:16:33 +0200

    added a test route

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -11,4 +11,8 @@ router.post("/create", upload.single('image'), BlogController.create);
 
 router.get("/getAll", BlogController.getAll);
 
+router.get("/", (req, res) => {
+    res.status(200).json({message: "hallo hallo hallo lololo lololo"});
+});
+
 export default router;
